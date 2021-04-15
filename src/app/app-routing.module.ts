945d1afc6b4a5d474470fdaa62f4485a425c409f
Vivek Shukla <vivek.shukla@hashedin.com>
2021-04-15 21:43:46 +0530

    Adding Property Search screen i.e. Second Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertySearchComponent } from './property-search/property-search.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'property/search/:address_id',component:PropertySearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertySearchHeaderComponent } from './property-search-header/property-search-header.component';
+import { PropertySearchComponent } from './property-search/property-search.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertySearchComponent,
+    PropertySearchHeaderComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
