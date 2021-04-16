cab30c5e2a7d8ded8d308d22ab45208131aa0337
Harshit Pal <harshit.pal@hashedin.com>
2021-04-16 18:42:42 +0530

    Reworked Landing page header to accomodate auth

--- src/app/landing-header/landing-header.component.ts
+++ src/app/landing-header/landing-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-landing-header',
@@ -9,15 +10,10 @@ export class LandingHeaderComponent implements OnInit {
 
   headerOpen = false;
 
-  // constructor() { }
-
-  // ngOnInit(): void {
-  // }
-
   images: string[] = ["/assets/images/header-background4.jpg" , "/assets/images/header-background2.jpg", "/assets/images/header-background3.jpg", "/assets/images/header-background7.jpg", "/assets/images/header-background5.jpg", "/assets/images/header-background6.jpg"];
   changeBackgroundCounter = 0;
   storedInterval: any;
-  constructor() {
+  constructor(public authService: AuthService) {
     this.storedInterval = setInterval(() => {
       this.changeBackgroundCounter = this.changeBackgroundCounter + 1;
       if (this.changeBackgroundCounter > this.images.length - 1) {
