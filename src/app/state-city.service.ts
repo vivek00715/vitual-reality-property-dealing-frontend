d0e1bf4540afa499dcc06feb149213b67db6bfc8
Harshit Pal <harshit.pal@hashedin.com>
2021-04-20 10:36:52 +0000

    City state

---
+++ src/app/state-city.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import * as cityDataJson from './citydata.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StateCityService {
+  data: {[e: string]: string[]} = (<any>cityDataJson).default;
+  getStates(): string[] {
+    console.log(this.data);
+    return Object.keys(this.data);
+  }
+
+  getCities(state: string): string[] {
+    return this.data[state];
+  }
+  constructor() {
+
+  }
+}
